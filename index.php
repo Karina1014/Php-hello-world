48328fb1ec3d41809793b9285f92951fd77f78fc
Karina1014 <janneth_wi15@hotmail.com>
2024-11-19 22:41:09 -0500

    feat: :memo: update
    Update file index.php

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-$port = getenv('PORT') ?: 8005; 
-echo "Hello, World! This message is generated using PHP without dockerfile.";
-
-
-$server = "0.0.0.0:$port"; 
-echo "Server running on port $port...\n";
+echo "Hello, World! This message is generated using PHP without Dockerfile.";
 ?>
